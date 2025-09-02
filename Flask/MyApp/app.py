d111491f73365b4120bedfbe26af858126d0c86d
Govinda Kumar <kumargovinda08@gmail.com>
2025-09-03 03:52:54 +0530

    Adding index page to app Health Tracker

--- Flask/MyApp/app.py
+++ Flask/MyApp/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create a Flask app instance
 app = Flask(__name__)
 
 # Define a route for the root URL
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+# def hello_world():
+#     return "Hello World!"
+
 
 # Run the app
 if __name__ == "__main__":
