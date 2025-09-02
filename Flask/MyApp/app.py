64f71c0b0679a1bc0bca723a20245522559382b7
Govinda Kumar <kumargovinda08@gmail.com>
2025-09-03 04:13:39 +0530

    Adding basic logic passage and representation in view

--- Flask/MyApp/app.py
+++ Flask/MyApp/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
 
-# Create a Flask app instance
 app = Flask(__name__)
 
-# Define a route for the root URL
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    user = {'username': 'Govinda'}
+    items = ['Flask', 'Django', 'FastAPI']
+    return render_template('index.html', user=user, items=items, title='Health Tracker')
 
-# def hello_world():
-#     return "Hello World!"
 
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
 
-# Run the app
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
